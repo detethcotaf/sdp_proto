e4d94e93013cb62a9db0e1e7f41777eec265ac55
Shuntaro Okuma <s-ohkuma@digitalblast.co.jp>
2022-03-06 02:46:45 +0000

    add satellite basemap

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sdp_proto
+
+
+- ReactもMapboxGLJSもDOMを扱うため、注意が必要
+- 以下のチュートリアルでどちらも使うための方法が理解できる
+
+    https://docs.mapbox.com/jp/help/tutorials/use-mapbox-gl-js-with-react/
+
+
+- Mapboxを使うためにはアカウントを作る必要がある
+
+    https://account.mapbox.com/auth/signup/?route-to=%22https://account.mapbox.com/%22
+
+    合わせてトークンも作成する
+
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicy1vaGt1bWEiLCJhIjoiY2wwZW1iM2V6MGc3bDNjbXp3d3ppNHFyciJ9.SbEhTUcPr41fp73LJqDdkg';
+
+
+export default function App() {
+    const mapContainerRef = useRef(null);
+    const [lng, setLng] = useState(138);
+    const [lat, setLat] = useState(35);
+    const [zoom, setZoom] = useState(5);
+
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/satellite-v9',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        // Add navigation control (the +/- zoom buttons)
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+        // mapが動いた時に稼働する
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4)); // getCenter: 地図の中心にあるポイントの新しい経度と緯度を取得
+            setLat(map.getCenter().lat.toFixed(4)); // toFixed: 桁数切り上げ
+            setZoom(map.getZoom().toFixed(2)); // getZoom: 地図に設定されているズームレベルを決定
+        });
+
+        // Clean up on unmount
+        return () => map.remove();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div className='map-container' ref={mapContainerRef} />
+        </div>
+    );
+}
